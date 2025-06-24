b1599e489babbd71aac22752b0fa0e107bf2acc9
ZeroX0368 <dabibanban@gmail.com>
2025-06-24 08:59:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 // Serve static files from the current directory
 app.use('/images', express.static(process.cwd()));
 
-const ai = new GoogleGenAI({ apiKey: "Gemini Apikey" });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDYrzBFmFiD39OrARGPKaDhqv9XsUaxlWw" });
 
 app.get("/api/gemini", async (req, res) => {
   try {
